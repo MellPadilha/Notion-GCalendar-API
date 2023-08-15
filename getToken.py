106df7d3617a5ff7adee1798eacf39c48e9bf6fa
Virrann <Virrann>
2023-08-15 06:46:15 -0300

    primeiros testes c/ googleAPI e getToken

---
+++ getToken.py
@@ -0,0 +1,10 @@
+from google_auth_oauthlib.flow import InstalledAppFlow
+import pickle
+
+def getToken():
+    scopes = ["https://www.googleapis.com/auth/calendar"]
+    flow = InstalledAppFlow.from_client_secrets_file("client_secret.json", scopes=scopes)
+    flow.run_local_server()
+    pickle.dump(flow.credentials, open("token.pkl", "wb"))
+
+    return pickle.load(open("token.pkl", "rb"))
